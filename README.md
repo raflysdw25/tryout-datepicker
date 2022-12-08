9303cdf9a3438282b65ac46b5e73115de449fbc9
raflysdw25 <34930411+raflysdw25@users.noreply.github.com>
2022-12-08 16:02:17 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tryout-datepicker
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vuepic-vue-datepicker-jzgg6y)
---
+++ src/components/Playground.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Datepicker
+      v-model="date"
+      :format="'dd-MM-yyyy'"
+      locale="id"
+      :day-names="['Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab', 'Min']"
+      auto-apply
+      :hide-navigation="['time']"
+      menu-class-name="custom-datepicker-style"
+    />
+    <Datepicker
+      style="margin-top: 20px"
+      v-model="date2"
+      month-picker
+      auto-apply
+      :hide-navigation="['time']"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+
+const date = ref();
+const date2 = ref();
+</script>
+
+<style>
+/* .custom-datepicker-style {
+  width: 400px;
+} */
+
+/* .dp__calendar_header */
+/* .dp__calendar */
+</style>
